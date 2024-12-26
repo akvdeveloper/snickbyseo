ba6e3dbedfcf75cd888483fc8ead2f288aa306d7
Amit Kumar Vishwakarma <115401399+akvdeveloper@users.noreply.github.com>
2024-12-26 15:10:27 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -28,7 +28,7 @@ function Header() {
   ];
 
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-gray-900 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse" onClick={closeMenu}>
           <Image
