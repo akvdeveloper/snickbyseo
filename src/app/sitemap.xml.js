953122bdb981285dd32fd21f5560cfddf36dcdbc
Amit Kumar <amit19910911@gmail.com>
2025-03-27 15:19:42 +0530

    canonical site add

---
+++ src/app/sitemap.xml.js
@@ -0,0 +1,32 @@
+export async function GET() {
+  const baseUrl = "https://snickbyseo.vercel.app";
+
+  // List of static pages
+  const staticRoutes = [
+    { url: baseUrl, lastModified: new Date() },
+    { url: `${baseUrl}/about`, lastModified: new Date() },
+    { url: `${baseUrl}/services`, lastModified: new Date() },
+    { url: `${baseUrl}/blog`, lastModified: new Date() },
+    { url: `${baseUrl}/contact`, lastModified: new Date() },
+  ];
+
+  // Generate XML sitemap
+  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${staticRoutes
+      .map(
+        (route) => `
+      <url>
+        <loc>${route.url}</loc>
+        <lastmod>${route.lastModified.toISOString()}</lastmod>
+      </url>`
+      )
+      .join("")}
+  </urlset>`;
+
+  return new Response(sitemapXml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
