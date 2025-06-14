60d8225f67f94321ab81b1dbb74d345611997f58
Amit Kumar <amit19910911@gmail.com>
2025-06-14 12:13:24 +0530

    Your commit message

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,23 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
         />
+         {/* Google Analytics gtag.js */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-R9HW0Z63NB`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-R9HW0Z63NB', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
         <meta name="google-site-verification" content="2sCwDPkJ_1OWhHA0d_SvehUA_dSyflXrfqs6divY3FU" />
         <meta name="msvalidate.01" content="658452A9348BF4978E00FC8F9D8C1BFE" />
         <meta name="yandex-verification" content="43966b377d50dabb" />
